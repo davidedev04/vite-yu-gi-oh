cf17339e4fe1bafda37ac01107db0cc30d3936d6
Davide Prencipe <davidedev04@gmail.com>
2024-01-08 16:27:30 +0100

    header completato

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<script>
+
+export default {
+    name: "AppHeader"
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="d-logo-span">
+            <img src="../assets/Yugioh_anime_logo.webp" alt="logo">
+            <span>
+                Yu-Gi-Oh Api
+            </span>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    width: 100vw;
+    height: 80px;
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+
+    .d-logo-span {
+        display: flex;
+        align-items: center;
+        margin-left: 20px;
+
+        img {
+            height: 30px;
+        }
+
+        span {
+            font-size: 30px;
+        }
+
+    }
+
+
+
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default{
+    name: "AppMain"
+}
+
+</script>
+
+<template>
+  
+    <main>
+
+    </main>
+
+</template>
+
+<style scoped>
+
+    main{
+        width: 100%;
+        height: calc(100vh - 80px);
+        background-color: #d48f38;
+    }
+
+</style>
